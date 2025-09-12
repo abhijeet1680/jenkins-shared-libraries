2ff50bcd587abc2200503c0b277fbdf0599c997a
abhijeet1680 <65946731+abhijeet1680@users.noreply.github.com>
2025-09-12 06:21:01 +0530

    Create code_checkout.groovy

---
+++ vars/code_checkout.groovy
@@ -0,0 +1,7 @@
+def call(String repoUrl, String branch = 'main') {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl]]
+    ])
+}
